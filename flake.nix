e95147c883285909a0ea2309eb52df9da730d057
Samuel Ahn <sya5jv@virginia.edu>
2025-12-19 02:11:38 +0000

    Removing ghostty and quickshell as Flake Packages

--- flake.nix
+++ flake.nix
@@ -10,14 +10,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ghostty.url = "github:ghostty-org/ghostty";
-
-    quickshell = {
-      # url = "github:outfoxxed/quickshell";
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
