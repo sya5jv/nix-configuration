5a9a8b9518034e1aaa137b1e2a12731d0790c535
Samuel Ahn <sya5jv@virginia.edu>
2026-04-10 21:56:38 -0400

    Updating noctalia.json

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -6,9 +6,9 @@
 
       inherit pkgs;
 
-      settings =
-        (builtins.fromJSON
-          (builtins.readFile ./noctalia.json)).settings;
+      # settings =
+      #   (builtins.fromJSON
+      #     (builtins.readFile ./noctalia.json)).settings;
 
     };  # packages.myNoctalia
 
