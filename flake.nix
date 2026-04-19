4db2f60cbcc49133e468015d245ffbdb2aca1902
Samuel Ahn <sya5jv@virginia.edu>
2026-04-19 19:43:37 -0400

    Adding Flake Input

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
     import-tree.url = "github:vic/import-tree";
 
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
+  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;}
+  (inputs.import-tree ./modules);
 }
